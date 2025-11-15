71f98dfc116a5633103e5aec9c3eb7fa9afc7383
imacek <imacek@gmail.com>
2025-11-14 22:51:25 -0800

    lock-free simulation now for better thread utilization and to avoid context switching

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,19 @@
 #include <thread>
 #include <semaphore>
 #include <mutex>
-#include <condition_variable>
+#include <atomic>
 
 using namespace std;
 
-constexpr int SIZE = 2000;
+constexpr int N = 2000;
 
 struct World {
-    bool Data[SIZE][SIZE];
+    bool Data[N][N];
 };
 
 void generateRandomNoise(World& world) {
-    for (int x = 0; x < SIZE; x++) {
-        for (int y = 0; y < SIZE; y++) {
+    for (int x = 0; x < N; x++) {
+        for (int y = 0; y < N; y++) {
             world.Data[x][y] = (rand() % 100) < 40;
         }
     }
@@ -27,7 +27,7 @@ void generateRandomNoise(World& world) {
 int countAliveAround(const World& world, int x, int y) {
     int count = 0;
 
-    static int steps[][2] = {
+    constexpr int steps[][2] = {
         { -1, -1 },
         { -1,  0 },
         { -1,  1 },
@@ -42,19 +42,19 @@ int countAliveAround(const World& world, int x, int y) {
     for (const auto & [dx, dy] : steps) {
         const int newX = x + dx;
         const int newY = y + dy;
-        if (0 <= newX && newX < SIZE && 0 <= newY && newY < SIZE) {
+        if (0 <= newX && newX < N && 0 <= newY && newY < N) {
             count += world.Data[newX][newY];
         } else {
-            count += world.Data[(SIZE + newX) % SIZE][(SIZE + newY) % SIZE];
+            count += world.Data[(N + newX) % N][(N + newY) % N];
         }
     }
 
     return count;
 }
 
-void simulateLifeStep(const World& worldNow, World& worldNext, int minX = 0, int maxX = SIZE) {
+void simulateLifeStep(const World& worldNow, World& worldNext, int minX = 0, int maxX = N) {
     for (int x = minX; x < maxX; x++) {
-        for (int y = 0; y < SIZE; y++) {
+        for (int y = 0; y < N; y++) {
             const int count = countAliveAround(worldNow, x, y);
 
             if (worldNow.Data[x][y]) {
@@ -76,70 +76,43 @@ World worlds[3];
 int simIndex = 0;
 int frameIndex = 0;
 
-constexpr int WORKER_COUNT = 16;
-mutex workerMutex[WORKER_COUNT];
-condition_variable workerCondVar[WORKER_COUNT];
-bool workerHasWork[WORKER_COUNT] = { false };
+constexpr int WORKER_COUNT = 10;
 
-struct default_binary_semaphore
-{
-    binary_semaphore sem{0}; //Make it public for easy access to the semaphore
-
-    constexpr default_binary_semaphore() : sem (0) {}
-    constexpr explicit default_binary_semaphore(auto count) : sem(count) {}
-};
-default_binary_semaphore workerStartSemaphores[WORKER_COUNT];
-default_binary_semaphore workerFinishSemaphores[WORKER_COUNT];
+atomic<bool> killSwitch {false};
+atomic<bool> workCanStart[WORKER_COUNT-1] {false};
+atomic<int> workFinishedCount {0};
 
 void simulateLoopWorker(const int wi) {
-    const int minX = wi * SIZE / WORKER_COUNT;
-    const int maxX = (wi + 1) * SIZE / WORKER_COUNT;
+    const int minX = wi * N / WORKER_COUNT;
+    const int maxX = (wi + 1) * N / WORKER_COUNT;
 
-    while (!WindowShouldClose()) {
-
-        workerStartSemaphores[wi].sem.acquire();
-        // {
-        //     unique_lock lock(workerMutex[wi]);
-        //     workerCondVar[wi].wait(lock, [wi] { return workerHasWork[wi]; });
-        // }
-
-        simulateLifeStep(worlds[simWorld1], worlds[simWorld2], minX, maxX);
-
-        workerFinishSemaphores[wi].sem.release();
-        // {
-        //     lock_guard lock(workerMutex[wi]);
-        //     workerHasWork[wi] = false;
-        //     workerCondVar[wi].notify_one();
-        // }
+    while (!killSwitch) {
+        if (bool yes = true; workCanStart[wi].compare_exchange_strong(yes, false)) {
+            simulateLifeStep(worlds[simWorld1], worlds[simWorld2], minX, maxX);
+            ++workFinishedCount;
+        }
     }
-
-    workerFinishSemaphores[wi].sem.release();
 }
 
 void simulateLoop() {
-
-    thread workers[WORKER_COUNT];
-    for (int wi = 0; wi < WORKER_COUNT; wi++) {
+    thread workers[WORKER_COUNT-1];
+    for (int wi = 0; wi < WORKER_COUNT-1; wi++) {
         workers[wi] = thread{simulateLoopWorker, wi};
     }
 
-    while (!WindowShouldClose()) {
+    constexpr int last_wi = WORKER_COUNT-1;
+    constexpr int minX = last_wi * N / WORKER_COUNT;
+    constexpr int maxX = (last_wi + 1) * N / WORKER_COUNT;
 
-        for (auto& workerStartSemaphore : workerStartSemaphores) {
-            workerStartSemaphore.sem.release();
+    while (!killSwitch) {
+        for (auto& wi : workCanStart) {
+            wi.store(true);
         }
 
-        // for (int wi = 0; wi < WORKER_COUNT; wi++) {
-        //     lock_guard lock(workerMutex[wi]);
-        //     workerHasWork[wi] = true;
-        //     workerCondVar[wi].notify_one();
-        // }
+        simulateLifeStep(worlds[simWorld1], worlds[simWorld2], minX, maxX);
 
-        for (auto& workerFinishSemaphore : workerFinishSemaphores) {
-            // unique_lock lock(workerMutex[wi]);
-            // workerCondVar[wi].wait(lock, [wi] { return !workerHasWork[wi]; });
-            workerFinishSemaphore.sem.acquire();
-        }
+        while (!killSwitch && workFinishedCount < WORKER_COUNT-1) { }
+        workFinishedCount = 0;
 
         {
             scoped_lock lock(worldPointersMutex);
@@ -159,10 +132,6 @@ void simulateLoop() {
         }
     }
 
-    for (auto& workerStartSemaphore : workerStartSemaphores) {
-        workerStartSemaphore.sem.release();
-    }
-
     for (auto& worker : workers) {
         worker.join();
     }
@@ -172,12 +141,12 @@ int main() {
     // Init Sim World
     generateRandomNoise(worlds[simWorld1]);
 
-    InitWindow(SIZE, SIZE, "Game Of Life");
+    InitWindow(N, N, "Game Of Life");
     //SetTargetFPS(64);
 
     thread simThread(simulateLoop);
 
-    const Image img = GenImageColor(SIZE, SIZE, BLACK);
+    const Image img = GenImageColor(N, N, BLACK);
     const Texture2D tex = LoadTextureFromImage(img);
 
     int localSimIndex = 0;
@@ -187,15 +156,15 @@ int main() {
         {
             scoped_lock lock(worldPointersMutex);
             renderWorld = simWorld1;
-
-            localSimIndex = simIndex;
         }
+
+        localSimIndex = simIndex;
 
         const auto& [Data] = worlds[renderWorld];
 
-        for (int x = 0; x < SIZE; x++) {
-            for (int y = 0; y < SIZE; y++) {
-                static_cast<Color*>(img.data)[x*SIZE + y] = Data[x][y] ? RED : DARKGREEN;
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; y < N; y++) {
+                static_cast<Color*>(img.data)[x*N + y] = Data[x][y] ? RED : DARKGREEN;
             }
         }
 
@@ -212,6 +181,8 @@ int main() {
 
         ++frameIndex;
     }
+
+    killSwitch = true;
 
     UnloadTexture(tex);
     UnloadImage(img);
